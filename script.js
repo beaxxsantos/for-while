19737736eb1c3cf56e8fb151ee4698513f8859cd
beaxxsantos <beasantos.rj@outlook.com>
2022-06-24 09:32:24 -0300

    if-else

---
+++ script.js
@@ -0,0 +1,21 @@
+for (let count = 1; count <= 10; count ++){
+    console.log(count)
+}
+
+for (let impar= 1; impar <=100; impar+=2){
+    console.log(impar)
+}
+
+let count = 1
+
+while(10 >= count){
+    console.log(count);
+    count ++;
+} 
+
+let par = 0
+
+while(100>= par){
+    console.log(par);
+    par +=2;
+}
